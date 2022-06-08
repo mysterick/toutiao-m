302cf46b667296b40990555b053aa56a93d412a5
carl <mysteric@yeah.net>
2022-06-08 12:12:09 +0800

    1.安装 vant 组件库 2.移动端 REM 适配（postcss-pxtorem 和 lib-flexible）

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import 'amfe-flexible' //加载动态设置 REM 基准值
 import './styles/index.less' //全局加载样式
+
+import Vant from 'vant' //加载 want 核心组件库
+import 'vant/lib/index.css' //加载 vant 全局样式
+
+Vue.use(Vant) //注册使用 vant 组件库
 
 Vue.config.productionTip = false
 
